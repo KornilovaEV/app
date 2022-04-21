f1fe6a8eaf694c1bce25e8a71aea94cc00913e0e
unknown <lenkakornilova2.com>
2022-04-21 14:49:46 +0300

    created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {useForm} from 'react-hook-form';
 
 function App() {
+  const {
+  register,
+  formState: {
+    errors, isValid,
+  },
+  handleSubmit,
+  reset,
+  } = useForm({
+    mode: "onBlur"
+  });
+
+const onSubmit = (data) => {
+  alert(JSON.stringify(data))
+  reset();
+}
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>Registration</h1>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <label>
+        FirstName:
+      <input 
+        {...register('firstName',{
+          required: "You need to fill out form!",
+          minLength: {
+            value: 2,
+            message: "Minimum of 2 characters!"
+          }
+        })}
+      />
+      </label>
+      <div style={{height: 40}}>
+        {errors?.firstName && <p>{errors?.firstName?.message || 'Error!'}</p>}
+      </div>
+      
+      <label>
+        LastName:
+      <input 
+        {...register('lastName',{
+          required: "You need to fill out form!",
+          minLength: {
+            value: 2,
+            message: "Minimum of 2 characters!"
+          }
+        })}
+      />
+      </label>
+      <div style={{height: 40}}>
+        {errors?.lastName && <p>{errors?.lastName?.message || 'Error!'}</p>}
+      </div>
+
+      <input type="submit" disabled={!isValid}/>
+    </form>
     </div>
   );
 }
